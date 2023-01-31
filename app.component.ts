34caed1e5918748c9ebe379fcfb3a291e4510bb1
R <sanjay.r@bt.com>
2023-01-31 15:16:48 +0530

    angular project

---
+++ app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  // template: `<h1>Hello world from inline template</h1>
+  // <p>Angular is awesome</p>
+  // `,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-app';
+}
